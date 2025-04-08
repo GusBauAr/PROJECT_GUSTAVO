8391fa7a0eb785806029248341cbc9c1cee08e73
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-04-08 10:32:44 -0400

    The code from the MUI (image list) was copied and pasted

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import CatImageList from './components/CatImageList';
+import { Container, Typography } from "@mui/material";
 
+const App: React.FC = () => {
+  return(
+    <Container>
+      <Typography>
+        GALERIA DE GATOS
+      </Typography>
+      <CatImageList />
+    </Container>
+
+  );
+
+};
+export default App;
--- src/services/catApi.ts
+++ src/services/catApi.ts
@@ -1,2 +1,4 @@
 const API_URL= 'https://api.thecatapi.com/v1/images/search';
 const API_KEY= 'live_LL32v5fsVv1g2FgSt4FjJgjRjTdYDFYiHyYakArGDwB2jiPB9Le5ZUpqVU4jG0M2';
+
+
